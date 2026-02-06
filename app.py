f0e31a027c02076fcfa17e39dfaa5e6d1733ace7
girinaidu2603 <girinaidu8688@gmail.com>
2026-02-07 03:55:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from flask import Flask, render_template
+import pandas as pd
 
+app = Flask(__name__)
+
+df = pd.read_csv("cleaned_crime_data.csv")
+
+@app.route("/")
+def home():
+    states = df['State'].unique()
+    return render_template("index.html", states=states)
+
+@app.route("/dashboard")
+def dashboard():
+    data = df.to_dict(orient='records')
+    return render_template("dashboard.html", data=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)

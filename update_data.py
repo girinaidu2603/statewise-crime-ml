56bb8a631963982ae562298f5f94b8940e856c32
girinaidu2603 <girinaidu8688@gmail.com>
2026-02-07 03:54:49 +0530

    Add functionality to update crime data from API
    Fetch crime data from API and save to CSV.

--- update_data.py
+++ update_data.py
@@ -1,1 +1,10 @@
+import requests
+import pandas as pd
 
+url = "https://data.gov.in/api/crime-data"
+response = requests.get(url)
+
+new_data = response.json()
+df_new = pd.DataFrame(new_data)
+
+df_new.to_csv("latest_crime_data.csv", index=False)
